9e20958beddd3dbd1da57fbe6ae1200f35f2feb8
Olivier BELTRANDO <olivierbeltrando@gmail.com>
2018-08-19 17:51:51 +0200

    fix: linux, toggle (home/target), pep8, pipenv

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from tkinter import *
 
 from tkinter.filedialog import askopenfilename, askdirectory
@@ -9,7 +11,7 @@ import tkinter.tix as tix
 import logging
 FORMAT = '%(asctime)-15s - %(levelname)s - %(message)s'
 logging.basicConfig(format=FORMAT)
-logger = logging.getLogger('GUI_list_files')
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 # ------------------------------------------------------
 
@@ -20,7 +22,7 @@ frameLabel = Frame(root, height=4)
 frameLabel.pack()
 label = Label(frameLabel, text='Give a name to your search')
 label.pack(side=LEFT)
-entry = Entry(frameLabel, bd= 5)
+entry = Entry(frameLabel, bd=5)
 entry.pack(side=RIGHT)
 
 frameDir = Frame(root)
@@ -29,37 +31,41 @@ labelDir = Label(frameDir, text="Dir to analyze")
 labelDir.pack(side=LEFT)
 dirnameVar = StringVar()
 
-entryDir = Label(frameDir, textvariable=dirnameVar, bd = 5)
+entryDir = Label(frameDir, textvariable=dirnameVar, bd=5)
 entryDir.pack(side=RIGHT)
 
 frameSelect = Frame(root)
 
+
 def askdir():
     dirName = askdirectory(parent=root)
-    print('selected is %s '% dirName)
+    print('selected is %s ' % dirName)
     dirnameVar.set(dirName)
     entryDir.setvar(name='text', value=dirName)
     return dirName
+
 
 button = Button(frameSelect, text="select folder", command=askdir)
 button.pack()
 frameSelect.pack()
 
+
 def process():
     import time
     import list_files
-    try :
+    try:
         t1 = time.time()
-        files = list_files.find_files(dirnameVar.get(), entry.get(), _simpleSearch=isSimpleSearch.get())
-        list_files.write_cvs(files, 'result.csv' )
+        files = list_files.find_files(
+            dirnameVar.get(), entry.get(), _simpleSearch=isSimpleSearch.get())
+        list_files.write_cvs(files, get_location())
         print()
         resultVar.set('Done in %d ms' % ((time.time() - t1)*1000))
-    except :
+    except:
         logger.exception('An exception occured during request')
-        resultVar.set('An error occured please see the comand line terminal' )
-    pass
+        resultVar.set('An error occured please see the comand line terminal')
 
-buttonProcess= Button(root, text="GO", command = process)
+
+buttonProcess = Button(root, text="GO", command=process)
 buttonProcess.pack()
 resultVar = StringVar()
 resultVar.set('waiting')
@@ -68,17 +74,40 @@ labelResult.pack()
 
 isSimpleSearch = IntVar()
 isSimpleSearch.set(1)
-#def toggleSimple () :
+# def toggleSimple () :
 #    if isSimpleSearch.get() :
 #        isSimpleSearch.set(0)
 #    else :
 #        isSimpleSearch.set(1)
-checkSimpleSearch = Checkbutton(root, text='Simple search', variable=isSimpleSearch)
+checkSimpleSearch = Checkbutton(
+    root, text='Simple search', variable=isSimpleSearch)
 checkSimpleSearch.pack()
+
+# -- TOGGLE BUTTON DEFAULT LOCATION ---
+result_location = IntVar()
+result_location.set(1)
+Radiobutton(root, text="Store result in Home",
+            variable=result_location, value=1).pack()
+Radiobutton(root, text="Store result in search folder",
+            variable=result_location, value=2).pack()
+
+
+def get_location():
+    if result_location.get() == 1:
+        from pathlib import Path
+        folder = Path.home()
+    else:
+        folder = dirnameVar.get()
+    return os.path.join(folder, "result.csv")
+# --
+
 
 def bindProcess(event):
     process()
 
-root.bind("<Return>", bindProcess)
-root.mainloop()
 
+root.bind("<Return>", bindProcess)
+try:
+    root.mainloop()
+except KeyboardInterrupt:
+    logger.info("Keyboard interrupt detected. Clean exit")
